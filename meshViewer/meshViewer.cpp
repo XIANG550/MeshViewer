94c1afb1959fa22a93f27caf881776dff7e1bd8a
unknown <xiang.gao.3@stonyrbook.edu>
2023-08-04 16:33:36 -0400

    first commit

---
+++ meshViewer/meshViewer.cpp
@@ -0,0 +1,187 @@
+﻿#include "window.h"
+#include "shader.h"
+
+#include <iostream>
+
+const char* vertexShaderSource = R"(
+		# version 330 core
+
+		layout (location = 0) in vec3 vPos;
+		layout (location = 1) in vec3 vColor;
+		//layout (location = 2) in vec2 vTexCoord;
+
+		out vec3 ourColor;
+		//out vec2 TexCoord;
+
+		//uniform mat4 transform; // uniform means applied to every point
+
+		void main(){
+            gl_Position = vec4(vPos, 1.0);
+            gl_Color = vec4(vColor, 1.0)
+			//gl_Position = transform * vec4(aPos, 1.0);
+			//ourColor = aColor; // set the color to the loaded vertex colors
+			//TexCoord = aTexCoord;
+		}
+	)";
+
+
+const char* fragmentShaderSource = R"( 
+		# version 330 core
+
+		out vec4 FragColor;
+
+		in vec3 vColor;
+		//in vec2 TexCoord;
+
+		// uniform sampler2D ourTexture;
+
+		void main() {
+            
+            FragColor = vec4(vColor, 1);
+
+		    // FragColor = vec4(ourColor, 1);
+			// FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+			// FragColor = texture(ourTexture, TexCoord);		
+		}
+	)";
+
+void processInput(GLFWwindow* window);
+
+int main()
+{
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // Create a window object of type CWindow
+    CWindow* window = new CWindow();
+    window->createWindow();
+
+    // Create a shader object of type CShader
+    CShader* shaderProgram = new CShader(vertexShaderSource, fragmentShaderSource);
+    if (!) {
+
+    }
+
+
+    // Shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    // render loop
+    while (!glfwWindowShouldClose(window->m_window))
+    {   
+	    // input
+	    processInput(window->m_window);
+
+	    // render
+	    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	    glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+       // glUseProgram(shaderProgram);
+        shaderProgram->use();
+
+
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+	    glfwSwapBuffers(window->m_window);
+	    glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    window->destroyWindow();
+    return 0;
+}
+
+
+void processInput(GLFWwindow* window) 
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
---
+++ meshViewer/render.h
@@ -0,0 +1,10 @@
+#ifndef _RENDER_H
+#define _RENDER_H
+
+//class CRender {
+//public:
+//	CRender()
+//};
+
+
+#endif
